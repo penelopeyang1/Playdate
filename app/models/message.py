ce1fe43fa459296047ae30c0d6977ff6edf84f40
Penelope Yang <penelope.yang21@gmail.com>
2024-08-22 07:54:22 -0500

    add prefix for prod user_id

--- app/models/message.py
+++ app/models/message.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Message(db.Model):
     __tablename__ = 'messages'
--- app/models/user_game.py
+++ app/models/user_game.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class UserGame(db.Model):
     __tablename__ = 'user_games'
@@ -7,7 +7,7 @@ class UserGame(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'), nullable=False)
 
     #RELATIONSHIPS
